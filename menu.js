bf776497b1d1a555afd398cbe45d98e140939e36
nusrathfathima <nusrathfathima567@gmail.com>
2021-04-06 23:31:54 -0400

    Added Search bar plugin

--- menu.js
+++ menu.js
@@ -1,30 +1,26 @@
-// $(document).ready(function(){
-//     $(".accordion").accordion({
-//       collapsible: true,
-//       heightStyle: "content"
-//     });
-//     // $(".slideshow").cycle({
-//       // 	fx: "fade" 
-//       // });
-//   });
-
-
-
-
 $(document).ready(function () {
   // jQuery FUNCTION.
   $("#accordion").accordion({
     heightStyle: "content",
     collapsible: true,
-    // event: "mouseover"
+  });
+
+  $(".fa-search").click(function(){
+    $(".icon").toggleClass("active");
+    $("input[type=text]").toggleClass("active");
   });
 });
 // EXPAND AND COLLAPSE MENUS ON “HEADER CLICK”
-$("#accordion h3").click(function () {
-  $("#accordion h3").animate({ 'background-color': '#FFF' }, 300);
-  $("#accordion h3").css({ 'color': '#000' });
+// $("#accordion h3").click(function () {
+//   $("#accordion h3").animate({ 'background-color': '#FFF' }, 300);
+//   $("#accordion h3").css({ 'color': '#000' });
 
-  // HIGHLIGHT THE SELECTED HEADER (BLOCK)
-  $(this).animate({ 'background-color': '#FA6305' }, 300);
-  $(this).css({ 'color': '#FFF' });
-});
+//   // HIGHLIGHT THE SELECTED HEADER (BLOCK)
+//   // $(this).animate({ 'background-color': '#FA6305' }, 300);
+//   // $(this).css({ 'color': '#FFF' });
+// });
+
+// $(".fa-search").click(function(){
+//   $(".icon").toggleClass("active");
+//   $("input[type=text]").toggleClass("active");
+// });
