d1dc9552bc3eafe8c93532003701a7329270096a
nusrathfathima <nusrathfathima567@gmail.com>
2021-04-09 22:06:32 -0400

    Adding Assignment3 code documentation

--- menu.js
+++ menu.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  // jQuery FUNCTION.
+  // Initializing jQuery Accordion with certain options 
   $("#accordion").accordion({
     heightStyle: "content",
     collapsible: true,
