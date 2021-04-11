bebfd451c185d57cab74a8c99b049b6770f7730d
nusrathfathima <nusrathfathima567@gmail.com>
2021-04-11 12:53:40 -0400

    Validation and correction

--- about.js
+++ about.js
@@ -1,4 +1,21 @@
 // Following function initializes the lazyload plugin
-$(document).ready(function(){
+$(document).ready(function () {
     $(".lazyload").lazyload();
 });
+
+// DOM Events mouseover and mouseout are called in this function
+window.onload = function () {
+    var x = document.getElementById("test");
+    x.addEventListener("mouseover", myFunction);
+    x.addEventListener("mouseout", newFunction);
+}
+
+// This function is implemented when the mouse is over the navigation links
+function myFunction(event) {
+    event.target.style.color = "#003333";
+}
+
+// This function gets implemented when mouse is not on the navigation links
+function newFunction(event) {
+    event.target.style.color = "";
+}
--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-// DOM click Event calling menuButton function
+// DOM Events click, mouseover and mouseout are called in this function
 window.onload = function () {
   var element = document.getElementById("button");
   element.addEventListener("click", menuButton);
-  };
+  var x = document.getElementById("test");
+  x.addEventListener("mouseover", myFunction);
+  x.addEventListener("mouseout", newFunction);
+};
 
 // Following function works when the button on the menu page is clicked and this function directs users to menu's page
 function menuButton() {
   location.href = "./menu.html";
 }
 
-// This function is ready when the document is loaded
-$(document).ready(function(){
+// This function is implemented when the mouse is over the navigation links
+function myFunction(event) {
+  event.target.style.color = "#003333";
+}
 
-// DOM each event to preload the images of popular items
-  $(".col-lg-4 img").each(function(){
-    var img = new Image();
-    img.src = $(this).attr("src");
-  })
+// This function gets implemented when mouse is not on the navigation links
+function newFunction(event) {
+  event.target.style.color = "";
+}
+
+// This function is ready when the document is loaded
+$(document).ready(function () {
 
   // Applying or initiating bxSlider plugin
   $("#slider-area").bxSlider({
-    auto:true,
+    auto: true,
     minSlides: 1,
     maxSlides: 1,
     captions: true,
